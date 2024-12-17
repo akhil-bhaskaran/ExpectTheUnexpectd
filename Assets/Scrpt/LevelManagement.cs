51a68439bca7075fb6ed5129e868d1d83216753f
Akhil C <akhilbhaskaran2004@gmail.com>
2024-12-17 11:43:12 +0530

    i dont know

--- Assets/Scrpt/LevelManagement.cs
+++ Assets/Scrpt/LevelManagement.cs
@@ -3,47 +3,95 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class LevelManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject pausePanel;
+    public GameObject canvasPrefab;  // Prefab of the Canvas
+    private GameObject canvasInstance; // Instance of the Canvas
+    private GameObject pausePanel;    // Reference to Pause Panel
+
     void Start()
     {
-        
-        
+        // Check if canvasPrefab is assigned
+        if (canvasPrefab != null)
+        {
+            Debug.Log("CanvasPrefab is assigned for this level.");
+
+            // Instantiate the Canvas prefab
+            canvasInstance = Instantiate(canvasPrefab, transform);
+            Debug.Log("Canvas Prefab instantiated successfully.");
+
+            // Find the PausePanel in the Canvas hierarchy
+            Transform panelTransform = canvasInstance.transform.Find("OptionsLabel");
+            if (panelTransform != null)
+            {
+                pausePanel = panelTransform.gameObject;
+                pausePanel.SetActive(false); // Ensure it starts inactive
+                Debug.Log("Pause Panel found and set to inactive.");
+            }
+            else
+            {
+                Debug.LogError("PausePanel not found in the Canvas Prefab! Check prefab structure.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Canvas Prefab is not assigned in the Inspector for this level!");
+        }
     }
+
+    
+
     public void gotoOne()
     {
         SceneManager.LoadScene("Level 1");
     }
+
     public void gotoTwo()
     {
         SceneManager.LoadScene("Level 2");
     }
-    public void gotoMainMenu() {
+
+    public void gotoMainMenu()
+    {
         SceneManager.LoadScene("HomePage");
         Time.timeScale = 1.0f;
     }
+
     public void restartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1.0f;
     }
-    public void stopGame() { 
 
-        pausePanel.SetActive(true);
-        Time.timeScale = 0f;
+    public void stopGame()
+    {
+        if (pausePanel != null)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+            Debug.Log("Game paused. PausePanel activated.");
+        }
+        else
+        {
+            Debug.LogError("PausePanel is not assigned or instantiated!");
+        }
     }
+
     public void resume()
     {
-
-        pausePanel.SetActive(false);
-        Time.timeScale = 1.0f;
+        if (pausePanel != null)
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1.0f;
+            Debug.Log("Game resumed. PausePanel deactivated.");
+        }
+        else
+        {
+            Debug.LogError("PausePanel is not assigned or instantiated!");
+        }
     }
-    // Update is called once per frame
+
     void Update()
     {
-        
     }
 }
